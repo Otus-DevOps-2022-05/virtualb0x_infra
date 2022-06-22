d99d9af1bf04d366ebf487a0658ef633245d6d86
Anastasiya Lomakova <anastasia.i.lomakova@tele2.ru>
2022-06-22 21:50:50 +0300

    Added scripts for start install and deploy

---
+++ startup_script.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+#Install Ruby
+sudo apt update && \
+sudo apt install -y ruby-full ruby-bundler build-essential && \
+ruby -v && \
+bundler -v
+
+#Install MongoDB
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - &&
+sudo bash -c 'echo ""deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodborg/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list'
+
+sudo apt update && \
+sudo apt install -y mongodb-org && \
+sudo systemctl start mongod && \
+sudo systemctl enable mongod && \
+sudo systemctl status mongod
+
+#Deploy app
+git clone -b monolith https://github.com/express42/reddit.git && \
+cd reddit && bundle install && \
+puma -d && \
+ps aux | grep puma

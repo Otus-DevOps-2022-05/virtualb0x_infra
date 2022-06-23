c46e39f4fc2960cd77c530356f07cd237b15a3eb
virtualb0x <68299651+virtualb0x@users.noreply.github.com>
2022-06-23 20:13:20 +0300

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,22 +1,19 @@
-#! /bin/sh
-#Install Ruby
-sudo apt update && \
-sudo apt install -y ruby-full ruby-bundler build-essential && \
-ruby -v && \
-bundler -v
+#!/bin/sh
 
-#Install MongoDB
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - &&
-sudo bash -c 'echo ""deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodborg/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list'
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo apt-get install -y ruby-full ruby-bundler build-essential git
 
-sudo apt update && \
-sudo apt install -y mongodb-org && \
-sudo systemctl start mongod && \
-sudo systemctl enable mongod && \
-sudo systemctl status mongod
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
-#Deploy app
-git clone -b monolith https://github.com/express42/reddit.git && \
-cd reddit && bundle install && \
-puma -d && \
-ps aux | grep puma
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+sudo systemctl enable mongod
+sudo systemctl start mongod
+
+sudo mkdir -p /opt/app
+cd /opt/app
+git clone -b monolith https://github.com/express42/reddit.git && cd reddit && bundle install
+puma -d

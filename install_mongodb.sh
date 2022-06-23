b6cddb17ba39c17b8d6b4e9044432f6c4d1fac06
virtualb0x <68299651+virtualb0x@users.noreply.github.com>
2022-06-23 12:38:57 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 sudo apt update && \
 sudo apt install -y mongodb && \
+sudo mv /lib/systemd/system/mongodb.service /lib/systemd/system/mongod.service \
 sudo systemctl start mongod && \
 sudo systemctl enable mongod && \
 sudo systemctl status mongod
